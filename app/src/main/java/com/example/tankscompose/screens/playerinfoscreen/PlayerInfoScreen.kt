ce57c26d2e02709ee3f2d391f90b77bd0b157618
grafgad <grafgad@mail.ru>
2023-02-08 22:39:43 +0300

    PlayersInfoScreen.kt

--- app/src/main/java/com/example/tankscompose/screens/playerinfoscreen/PlayerInfoScreen.kt
+++ app/src/main/java/com/example/tankscompose/screens/playerinfoscreen/PlayerInfoScreen.kt
@@ -1,7 +1,68 @@
 package com.example.tankscompose.screens.playerinfoscreen
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.tankscompose.theme.TanksTheme
 
 @Composable
-fun PlayerInfoScreen() {
+fun PlayerInfoScreen(
+    modifier: Modifier = Modifier
+) {
+    Column(
+        modifier = modifier
+            .fillMaxSize()
+            .background(TanksTheme.color.background)
+            .statusBarsPadding()
+    ) {
+        Column(
+            modifier = Modifier.fillMaxWidth(),
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Text(text = "Player Name")
+            Spacer(modifier = Modifier.height(10.dp))
+            Text(text = "Rating")
+            Spacer(modifier = Modifier.height(10.dp))
+            Text(text = "Win percent")
+            Spacer(modifier = Modifier.height(10.dp))
+        }
+        Column(
+            modifier = Modifier.fillMaxWidth(),
+            horizontalAlignment = Alignment.Start
+        ) {
+            Text(text = "in game since")
+            Spacer(modifier = Modifier.height(10.dp))
+            Text(text = "last battle")
+            Spacer(modifier = Modifier.height(10.dp))
+            Text(text = "battles played")
+            Spacer(modifier = Modifier.height(10.dp))
+            Text(text = "max destroyed")
+            Spacer(modifier = Modifier.height(10.dp))
+            Text(text = "max experience")
+            Spacer(modifier = Modifier.height(10.dp))
+            Text(text = "hits percent")
+            Spacer(modifier = Modifier.height(10.dp))
+            Text(text = "avg experience")
+            Spacer(modifier = Modifier.height(10.dp))
+            Text(text = "wins")
+            Spacer(modifier = Modifier.height(10.dp))
+            Text(text = "draws")
+            Spacer(modifier = Modifier.height(10.dp))
+            Text(text = "spotted enemies")
+            Spacer(modifier = Modifier.height(10.dp))
+            Text(text = "trees felled")
+            Spacer(modifier = Modifier.height(10.dp))
+        }
+    }
+}
+
+@Preview
+@Composable
+fun PlayerInfoScreenPreview() {
+    PlayerInfoScreen()
 }
